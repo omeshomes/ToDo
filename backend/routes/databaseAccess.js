a1c32172d1e7defd151042237b53756ba93e71b6
omeshomes <omid.rooholfada@yale.edu>
2017-06-30 16:21:31 -0700

    mongoDB connection

---
+++ backend/routes/databaseAccess.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const router = express.Router();
+import TodoItem from '../models/TodoItem.js'
+
+router.get('/add', (req, res) => {
+  const testTodo = new TodoItem({
+    task: "test task"
+  });
+
+  testTodo.save()
+    .then(response => {
+      res.send(response);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+  });
+
+export default router;
