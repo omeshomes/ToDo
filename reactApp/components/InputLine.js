2d54c105d1b35b42e00dd5357846bd1d6661fca2
omeshomes <omid.rooholfada@yale.edu>
2017-06-30 14:57:11 -0700

    input field works

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class InputLine extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      typedText: ''
+    }
+  }
+
+  handleTyping(event) {
+    this.setState({typedText: event.target.value});
+  }
+
+  handleSubmit() {
+    this.props.submit(this.state.typedText);
+    this.setState({typedText: ''});
+  }
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="task" />
-        <button type="button" onClick={() => this.props.submit("Test Task")}>Add todo</button>
+        <input type="text" placeholder="task" onChange={(event) => this.handleTyping(event)} value={this.state.typedText}/>
+        <button type="button" onClick={() => this.handleSubmit()}>Add todo</button>
       </div>
     )
   }
